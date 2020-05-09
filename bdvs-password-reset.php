4124c7a0ba7cb5d82a1e14f70fcd76c17aa558b1
Dominic Vermeulen-Smith <contact@bedevious.co.uk>
2020-05-09 10:53:34 +0100

    updated version tag

--- bdvs-password-reset.php
+++ bdvs-password-reset.php
@@ -7,7 +7,7 @@
 * Plugin Name: REST API Password Reset with Code
 * Plugin URI: https://www.bedevious.co.uk/
 * Description: Allow users to reset their password using a random 4 digit code via the REST API
-* Version: 0.0.1
+* Version: 0.0.2
 * Author: Be Devious Web Development
 * Author URI: https://www.bedevious.co.uk/
 * License: GNU GPLv3

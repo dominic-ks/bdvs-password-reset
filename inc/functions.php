8f76ad02c187562ad0c587eabdfb1a59396e8faf
Dominic Vermeulen-Smith <contact@bedevious.co.uk>
2020-06-12 09:33:14 +0100

    added support for wp_timezone() for WP versions < 5.2.0

--- inc/functions.php
+++ inc/functions.php
@@ -98,3 +98,63 @@ function bdpwr_get_formatted_date( $time = false ) {
 function bdpwr_get_user( $user_id = false ) {
   return new BDPWR_User( $user_id );
 }
+
+
+/**
+*
+* BACKWARDS COMPATIBILITY FILLS
+*
+* The following declares new functions available from WP 5.3.0
+* in the case that these have not already been declared, i.e. WP is < 5.3.0
+*
+**/
+
+/**
+*
+* Retrieves the timezone from site settings as a string.
+*
+* Uses the `timezone_string` option to get a proper timezone if available,
+* otherwise falls back to an offset.
+*
+* @since 5.3.0
+*
+* @return string PHP timezone string or a ±HH:MM offset.
+*
+**/
+
+if( ! function_exists( 'wp_timezone_string' )) {
+  function wp_timezone_string() {
+    $timezone_string = get_option( 'timezone_string' );
+
+    if ( $timezone_string ) {
+      return $timezone_string;
+    }
+
+    $offset  = (float) get_option( 'gmt_offset' );
+    $hours   = (int) $offset;
+    $minutes = ( $offset - $hours );
+
+    $sign      = ( $offset < 0 ) ? '-' : '+';
+    $abs_hour  = abs( $hours );
+    $abs_mins  = abs( $minutes * 60 );
+    $tz_offset = sprintf( '%s%02d:%02d', $sign, $abs_hour, $abs_mins );
+
+    return $tz_offset;
+  }
+}
+
+/**
+*
+* Retrieves the timezone from site settings as a `DateTimeZone` object.
+*
+* Timezone can be based on a PHP timezone string or a ±HH:MM offset.
+*
+* @return DateTimeZone Timezone object.
+* 
+**/
+
+if( ! function_exists( 'wp_timezone' )) {
+  function wp_timezone() {
+    return new DateTimeZone( wp_timezone_string() );
+  }
+}
